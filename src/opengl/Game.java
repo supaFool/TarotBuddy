77e163534613f2d65766f4da847167e76e773665
Erick <supafool@live.com>
2018-07-23 21:17:59 -0400

    Getting ready for OpenGl
    Just got the Classpath set properly. Ready to roll with Slick

---
+++ src/opengl/Game.java
@@ -0,0 +1,46 @@
+package opengl;
+
+
+import org.newdawn.slick.*;
+
+import java.util.logging.Level;
+        import java.util.logging.Logger;
+
+/**
+ * This Class is the Main Class for the new starting branch on OpenGl
+ */
+
+public class Game extends BasicGame
+{
+    public Game(String gamename)
+    {
+        super(gamename);
+    }
+
+    @Override
+    public void init(GameContainer gc) throws SlickException {}
+
+    @Override
+    public void update(GameContainer gc, int i) throws SlickException {}
+
+    @Override
+    public void render(GameContainer gc, Graphics g) throws SlickException
+    {
+        g.drawString("Howdy!", 100, 10);
+    }
+
+    public static void main(String[] args)
+    {
+        try
+        {
+            AppGameContainer appgc;
+            appgc = new AppGameContainer(new Game("Simple Slick Game"));
+            appgc.setDisplayMode(640, 480, false);
+            appgc.start();
+        }
+        catch (SlickException ex)
+        {
+            Logger.getLogger(Game.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
