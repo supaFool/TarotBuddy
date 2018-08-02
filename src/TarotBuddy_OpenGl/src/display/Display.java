d4b9554ae647dfc9be56ca29213e8aca42edc068
Erick <supafool@live.com>
2018-08-02 17:14:13 -0400

    Images are working
    Got the call correct to read images, dont think they are buffered though

--- src/TarotBuddy_OpenGl/src/display/Display.java
+++ src/TarotBuddy_OpenGl/src/display/Display.java
@@ -2,9 +2,7 @@ package display;
 
 import card.CardData;
 import logic.CardLogic;
-import org.newdawn.slick.Color;
-import org.newdawn.slick.GameContainer;
-import org.newdawn.slick.Graphics;
+import org.newdawn.slick.*;
 import org.newdawn.slick.geom.Polygon;
 import org.newdawn.slick.geom.Rectangle;
 import org.newdawn.slick.geom.Shape;
@@ -13,27 +11,33 @@ import org.newdawn.slick.geom.Shape;
  * Created by supaFool on 7/27/2018.
  */
 public class Display {
-    private Graphics g;
-    private String currentText;
-    private GameContainer gc;
     float currentX;
     float currentY;
+    Image i;
 
 
 
     public Display(GameContainer gc) {
-        this.gc = gc;
+        init();
     }
 
     private void init(){
         currentX = 10;
         currentY = 0;
+        try {
+            i = new Image("card/image/decks/1910/1.jpg");
+        } catch (SlickException e) {
+            e.printStackTrace();
+        }
     }
 
     public void render(Graphics g){
         currentX += 0.05;
         currentY += 0.05;
-        g.draw(new Rectangle(currentX,currentY,10,10));
-        g.drawGradientLine(0.5f,1f,0.5f,1f,.32f,1f,125f *3,125f *3,0.75f,0.2f,0.8f,1);
+//        g.draw(new Rectangle(currentX,currentY,10,10));
+//        g.drawGradientLine(0.5f,1f,0.5f,1f,.32f,1f,125f *3,125f *3,0.75f,0.2f,0.8f,1);
+        g.drawImage(i, currentX,currentY);
+        g.setColor(Color.red);
+        g.drawOval(currentX,currentY, 5, 5);
     }
 }
